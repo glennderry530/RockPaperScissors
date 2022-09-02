5898a2e656b47269d7470e05af81ee39636ac5f1
glennderry530 <glennderry@gmail.com>
2022-09-02 09:12:38 -0700

    add game mode and change math to determine winner

--- javascript.js
+++ javascript.js
@@ -1,37 +1,69 @@
 const items = ["rock", "paper", "scissors"];
-let computerSelection = getComputerChoice(items);
 
 function getComputerChoice(items) {
     return items[Math.floor(Math.random()*items.length)];
 }
 
 function validatePlayerInput(playerInput, items){
-    console.log (items.includes(playerInput.toLowerCase()));
     if (items.includes(playerInput.toLowerCase())) {
         return playerInput.toLowerCase();
-    } else return ("  Invalid Input !  ");
+    } else return ("  INVALID YOU LOOSE THIS ROUND!  "); // Add logic to loose round to computer
    }
 
+  
 function playRound(playerSelection, computerSelection) {
-    const win = ("You win -> " + playerSelection + " beats " + computerSelection);
-    const loose = ("You Loose -> " + playerSelection + " looses to " + computerSelection);
-    const tie = ("Its a Tie -> " + playerSelection + " is equal to " + computerSelection); 
-    if (playerSelection === "rock" && computerSelection === "rock") {
-        console.log(tie); 
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log(win); 
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log(loose); 
-    } else if (playerSelection === "paper" && computerSelection === "paper") {
-        console.log(tie); 
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log(loose); 
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log(win); 
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log(loose); 
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log(win); 
-    } else if (playerSelection === "scissors" && computerSelection === "scissors")
-        console.log(tie);
+    console.log("Computer Picked -> " + computerSelection + " " + items.indexOf(computerSelection));
+    console.log("Player Picked -> " + playerSelection + " " + items.indexOf(playerSelection));
+    
+
+    const win = (playerSelection + " beats " + computerSelection);
+    const loose = (playerSelection + " looses to " + computerSelection);
+    const tie = (playerSelection + " is equal to " + computerSelection); 
+
+    let roundResult = items.indexOf(computerSelection) - items.indexOf(playerSelection);
+
+   if (roundResult == 0) {
+        console.log(tie); return ("Tie");
+    } else if (roundResult == -1) {
+        console.log(win); return ("Player");
+    } else if (roundResult == 1) {
+        console.log(loose); return ("Computer");
+    } else if (roundResult == 2) {
+        console.log(win); return ("Player");
+    } else if (roundResult == -2) {
+        console.log(loose); return ("Computer");
+    };
+    
     }
+
+function game() {
+    for (let i = 0; i < 5; i ++) {
+        console.log ("Game # " + (i+1) + " of 5 ");
+
+        let playerInput = prompt("Rock, Paper, or Scissors?");
+        let playerSelection = validatePlayerInput(playerInput, items);
+
+        let computerSelection = getComputerChoice(items);
+
+        let roundWinner = playRound(playerSelection, computerSelection);
+        console.log(roundWinner);
+        scoreBoard(roundWinner);
+    }
+}
+
+let playerScore = 0; let computerScore = 0;
+
+    function scoreBoard (roundWinner) {
+
+        if (roundWinner = ("Tie")) {
+            console.log("Its a Tie! " + " Score is: Player [" + playerScore + "] Computer [" + computerScore + "]");
+        } else if (roundWinner = ("Player")) {
+            playerScore ++; console.log("Player Wins! " + " Score is: Player [" + playerScore + "] Computer [" + computerScore + "]");
+        } else if (roundWinner = ("Computer")) {
+            computerScore ++; console.log("Computer Wins! " + " Score is: Player [" + playerScore + "] Computer [" + computerScore + "]");
+        }
+    }
+
+
+
+
