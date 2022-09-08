9ded03fff354dd964e99b5d95fae84c741e4826f
glennderry530 <glennderry@gmail.com>
2022-09-08 13:00:23 -0700

    Add buttons for player input, results now in DOM

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RockPaperScissors
 Odin Rock Paper Scissors Game
+This is the UI Version
--- javascript.js
+++ javascript.js
@@ -4,18 +4,16 @@ function getComputerChoice(items) {
     return items[Math.floor(Math.random()*items.length)];
 }
 
-// FIX THIS SECTION SO THAT IT WONT ALLOW an Undefined value out of the function (you have to type correctly)
-function getPlayerInput() {
-    let playerInput = prompt("Please Enter: Rock, Paper, or Scissors");
-    if (items.includes(playerInput.toLowerCase())) {
-     return playerInput.toLowerCase();
-    } else alert("NOT A VALID INPUT - TRY AGAIN"); getPlayerInput();
-}
-
 function playRound(playerSelection, computerSelection) {
-    console.log("Computer Picked -> " + computerSelection + " " + items.indexOf(computerSelection));
-    console.log("Player Picked -> " + playerSelection + " " + items.indexOf(playerSelection));
-    
+    const compPick = document.getElementById('compPick');
+    compPick.style.color = 'red';
+    compPick.textContent = ("Computer Picked -> " + computerSelection);
+    gameOutput.appendChild(compPick);
+
+    const playerPick = document.getElementById('playerPick');
+    playerPick.style.color = 'blue';
+    playerPick.textContent = ("Player Picked -> " + playerSelection);
+    gameOutput.appendChild(playerPick);
 
     const win = (playerSelection + " beats " + computerSelection);
     const loose = (playerSelection + " looses to " + computerSelection);
@@ -38,30 +36,41 @@ function playRound(playerSelection, computerSelection) {
 
 
 function game() {
-    for (let i = 0; i < 5; i ++) {
-        console.log ("Game # " + (i+1) + " of 5 ");
+  // for (let i = 0; i < 5; i ++) {
+  //     console.log ("Game # " + (i+1) + " of 5 ");
 
-        let playerSelection = getPlayerInput();
+  //      let playerSelection = getPlayerInput();
         
-        let computerSelection = getComputerChoice(items);
+  //      let computerSelection = getComputerChoice(items);
 
-        let roundWinner = playRound(playerSelection, computerSelection);
+  //      let roundWinner = playRound(playerSelection, computerSelection);
        
-        scoreBoard(roundWinner);
-         }
-    }
+  //      scoreBoard(roundWinner);
+       //  }
+}
 
 
 let playerScore = 0; let computerScore = 0;
 
-    function scoreBoard (roundWinner) {
+function scoreBoard(roundWinner) {
 
-        if (roundWinner == ("Tie")) {
-            console.log("Its a Tie!");
-        } else if (roundWinner == ("Player")) {
-            playerScore++; console.log("Player Wins! ");
-        } else if (roundWinner == ("Computer")) {
-            computerScore++; console.log("Computer Wins! ");
-        }
-        console.log("SCORE: Player[" + playerScore + "] Computer[" + computerScore + "]");
+    const rW = document.getElementById("roundWinner");
+
+    if (roundWinner == ("Tie")) {
+        rW.textContent = ("It's a TIE! ");
+        gameOutput.appendChild(rW);
+    } else if (roundWinner == ("Player")) {
+        playerScore++; 
+        rW.textContent = ("Player Wins! "); 
+        gameOutput.appendChild(rW);
+    } else if (roundWinner == ("Computer")) {
+         computerScore++; 
+         rW.textContent = ("Computer Wins! "); 
+         gameOutput.appendChild(rW);
     }
+    const sB = document.getElementById ("scoreBoard");
+    sB.style.color = 'yellow'; sB.style.backgroundColor = 'black';
+    sB.textContent = ("SCORE: Player[" + playerScore + "] Computer[" + computerScore + "]")
+    gameOutput.appendChild(sB);
+
+}
